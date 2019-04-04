f7704c179c80f3d9678d8df6ea75bec35f3e2ef7
DavidTan1 <dtan2785@bths.edu>
2019-04-04 03:50:31 -0400

    code

--- js/game.js
+++ js/game.js
@@ -1,56 +1,69 @@
-	gameScene = new Phaser.Scene('Game');
+	//gameScene = new Phaser.Scene('Game');
+
 	//create the game
 
 	//choose the size of the map
-	config = {
-		type: Phaser.AUTO,
-		width: 640,
-		height: 360,
-		scene: gameScene
-	};
-
-	game = new Phaser.Game(config);
+	// config = {
+	// 	type: Phaser.AUTO,
+	// 	width: 640,
+	// 	height: 360,
+	// 	scene: gameScene
+	// };
 
 
-	gameScene.preload = function(){
-		this.load.image('background', 'assets/background.png')
-		this.load.image('player', 'assets/player.png')
-		this.load.image('plant','assets/Tulip_Stage_1.png')
-		this.load.image('plant1','assets/Tulip_Stage_2.png')
-		this.load.image('plant2','assets/Tulip_Stage_3.png')
-		this.load.image('plant3','assets/Tulip_Stage_4.png')
-		this.load.image('plant5','assets/Tulip_Stage_6.png')
-		this.load.image('blank', 'assets/dirt.png')
 
+	//game = new Phaser.Game(config);
+
+
+
+	game = new Phaser.Game(640, 360, Phaser.AUTO, 'phaser', { preload: preload, create: create, update: update });
+
+
+
+
+	function preload(){
+		game.load.image('background', 'assets/background.png')
+		game.load.image('player', 'assets/player.png')
+		game.load.image('plant','assets/Tulip_Stage_1.png')
+		game.load.image('plant1','assets/Tulip_Stage_2.png')
+		game.load.image('plant2','assets/Tulip_Stage_3.png')
+		game.load.image('plant3','assets/Tulip_Stage_4.png')
+		game.load.image('plant5','assets/Tulip_Stage_6.png')
+		game.load.image('blank', 'assets/dirt.png')
 	}
 
 	
 	var listOfPlants = [];
-	
-	var right;
-	var plant;
-	var kill;
-	var left;
-	var up;
-	var down;
+
+	var group2;
 
 
-	gameScene.create = function(){
+	function create(){
 
-		let bg = this.add.sprite(0, 0, 'background');
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+
+		bg = game.add.sprite(0, 0, 'background');
 		// origin is originally set to center, change origin to the top-left of the sprite
-		bg.setOrigin(0,0);
+		//bg.setOrigin(0,0);
 
 		//this.background = this.add.sprite(0,0,'background')
 	
 	
-		this.plant = this.add.group(); //creating a new plant group
-		this.plant.enableBody = true; //allows interaction between the plant and user
-		this.plant.physicsBodyType = Phaser.Physics.ARCADE;//one of the three types of groups
-		
-	
-		player = this.add.sprite(70, 180, 'player') //adds the image to the field
+		game.plant = game.add.group(); //creating a new plant group
+		game.plant.enableBody = true; //allows interaction between the plant and user
+		game.plant.physicsBodyType = Phaser.Physics.ARCADE;//one of the three types of groups
 
+
+
+		group2 = game.add.group();
+
+
+		player = game.add.sprite(70, 180, 'player') //adds the image to the field
+		game.physics.enable(player, Phaser.Physics.ARCADE);
+
+
+		group2.add(player);
 
 
 
@@ -61,9 +74,9 @@
 		listOfPlants.push(wildflower1)
 		listOfPlants.push(wildflower2)
 
+
 		//create random amount of crops
 		testNum = Math.floor(Math.random() * 7) + 1
-
 
 
 		for(var x=0; x<testNum; x++){
@@ -72,94 +85,103 @@
 		}
 		
 		//this.add.sprite(listOfPlants[0].plant.x, listOfPlants[0].plant.y, 'plant')
-		
 
-		right = gameScene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-		left = gameScene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-		up = gameScene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-		down = gameScene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-		plant = gameScene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
-		kill = gameScene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K);
+		this.plant = game.input.keyboard.addKey(Phaser.Keyboard.P);
 
+		this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+		this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+		this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+		this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+		this.killKey = game.input.keyboard.addKey(Phaser.Keyboard.K);
 
 	}
 
-	
-	//continously update the game
-	gameScene.update = function(){
 
-		if(kill.isDown & !plant.isDown & !down.isDown & !up.isDown & !left.isDown & !right.isDown)
+	//continously update the game
+	function update(){
+
+		if (this.leftKey.isDown & player.x >= 0)
+		{
+			player.x -= 1
+			grow()
+			//player.body.velocity.x = -200;
+		}
+
+		if (this.rightKey.isDown & player.x <= 640)
+		{
+			player.x += 1
+			grow()
+
+		}
+
+		if(this.killKey.isDown & !this.plant.isDown & !this.downKey.isDown & !this.upKey.isDown & !this.leftKey.isDown & !this.rightKey.isDown)
 		{
 			for(var i = 0; i<listOfPlants.length; i++){
 				killplant(listOfPlants[i])
 			}
 		}
 
-			if (right.isDown){
-			if(player.x <= 640){
-				player.x += 1
-				grow()
-			}
-		}
 
-		if (left.isDown){
-			if(player.x >= 0){
-				player.x -= 1
-				grow()
-			}
-		}
 
-		if (up.isDown){
+		if (this.downKey.isDown){
 			if(player.y >= 0){
-				player.y -= 1
-				grow()
-			}
-		}
-
-		if (down.isDown){
-			if(player.y <= 360){
 				player.y += 1
 				grow()
 			}
 		}
 
-		if (plant.isDown & !down.isDown & !up.isDown & !left.isDown & !right.isDown){
+
+
+		if (this.upKey.isDown){
+			if(player.y <= 360){
+				player.y -= 1
+				grow()
+			}
+		}
+
+		if(this.plant.isDown & !this.downKey.isDown & !this.upKey.isDown & !this.leftKey.isDown & !this.rightKey.isDown)
+		{
+			alert("asda")
 			plantplant(player)
 		}
 
 		for(var x = 0; x<listOfPlants.length; x++){
 			if(listOfPlants[x].death == false){
 				if(listOfPlants[x].age == 0){
-					this.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'plant')
+					game.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'plant')
 
 				}
 				if(listOfPlants[x].age == 100){
-					this.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'plant1')
+					game.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'plant1')
 				}
 				if(listOfPlants[x].age == 200){
-					this.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'plant2')
+					game.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'plant2')
 				}
 				if(listOfPlants[x].age == 300){
-					this.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'plant3')
+					game.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'plant3')
 				}
-				if(listOfPlants[x].age == 400){
-					this.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'plant5')
+				if(listOfPlants[x].age == 500){
+					game.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'plant5')
 				}
 			}else{
-				this.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'blank')
+				game.add.sprite(listOfPlants[x].x, listOfPlants[x].y,'blank')
 			}
 		}
+
+		game.world.bringToTop(group2);
+
 	}
 	
 
-	grow = function(){
+	 function grow(){
 		for(var y = 0; y<listOfPlants.length; y++){
 			if(!listOfPlants[y].death)
 				listOfPlants[y].age++
 		}
 	}
 
-	killplant = function(crop){
+	 function killplant(crop){
 		//you can step on your own crop and kill it
 		if(player.x>crop.x-15 & player.x<crop.x+15)
 			if(player.y>crop.y-15 & player.y<crop.y+15)
@@ -169,7 +191,7 @@
 
 
 	
-	wateringplant = function(crop, user){
+	 function wateringplant(crop, user){
 		//doesn't check for left or right
 		distance = Phaser.Math.Distance.Between(user.x, user.y , crop.plant.x , crop.plant.y)
 
@@ -190,7 +212,7 @@
 
 	}
 
-	plantplant = function(user){
+	 function plantplant(user){
 		var flower = new createPlant(false, 0, user, false, user.x+1, user.y+1)
 		listOfPlants.push(flower)
 	}	
